d8bc3ae8dbd39c9694a9d4e5649fffea865371cc
Mobitaps <mobitaps.dev@gmail.com>
2015-11-19 23:24:14 +0700

    layout for weather of current time

---
+++ app/src/main/java/com/demo/theweather/util/TemperatureUtils.java
@@ -0,0 +1,11 @@
+package com.demo.theweather.util;
+
+/**
+ * Created by Jackie Nguyen <nguyenngoc100@gmail.com> on 11/19/15.
+ */
+public class TemperatureUtils {
+
+    public static String format(double temperature) {
+        return String.valueOf(Math.round(temperature)) + "°";
+    }
+}
