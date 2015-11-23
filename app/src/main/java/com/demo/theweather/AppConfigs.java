88b6583f90097a7298d50bc290eca3c6d9b77bcc
Megataps <mobitaps.dev@gmail.com>
2015-11-23 16:20:32 +0700

    change weather api key

--- app/src/main/java/com/demo/theweather/AppConfigs.java
+++ app/src/main/java/com/demo/theweather/AppConfigs.java
@@ -9,8 +9,8 @@ package com.demo.theweather;
 public class AppConfigs {
 
     public static final int NUMBER_OF_RESULT = 10;
-//    public static final String WEATHER_API_KEY = "6d06579b6dc6d1d259adb0091869a";
-    public static final String WEATHER_API_KEY = "ede21df05a021aa9768352a93e6b4";
+    public static final String WEATHER_API_KEY = "6d06579b6dc6d1d259adb0091869a";
+//    public static final String WEATHER_API_KEY = "ede21df05a021aa9768352a93e6b4";
     public static final String APP_URL = "http://api.worldweatheronline.com/free/v2/";
 
     public static final String WEATHER_URL = APP_URL + "weather.ashx?q=%s&format=json&num_of_results=%d&key=%s";
